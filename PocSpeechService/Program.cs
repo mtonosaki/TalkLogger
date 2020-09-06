dba386f6e22c86b9ca6a2c2cd80b447facb38ae3
Manabu Tonosaki <manabu@tomarika.com>
2020-09-06 15:25:23 +0900

    Ver 1.0

---
+++ PocSpeechService/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using PocSpeechService;
+
+namespace Speech.Recognition
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            Console.WriteLine("Talk to mic...");
+            await RecognizeSpeechAsync();
+        }
+
+        static async Task RecognizeSpeechAsync()
+        {
+            var param = new MySecretParameter();
+            var config = SpeechConfig.FromSubscription(param.SubscriptionKey, param.ServiceRegion);
+
+            using var recognizer = new SpeechRecognizer(config);
+
+            var result = await recognizer.RecognizeOnceAsync();
+            switch (result.Reason)
+            {
+                case ResultReason.RecognizedSpeech:
+                    Console.WriteLine($"We recognized: {result.Text}");
+                    break;
+                case ResultReason.NoMatch:
+                    Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                    break;
+                case ResultReason.Canceled:
+                    var cancellation = CancellationDetails.FromResult(result);
+                    Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                    if (cancellation.Reason == CancellationReason.Error)
+                    {
+                        Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                        Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                        Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                    }
+                    break;
+            }
+        }
+    }
+}
