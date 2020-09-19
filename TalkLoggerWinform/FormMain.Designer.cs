01ef9dfc975634d3da88c825db593630ddef7cc9
Manabu Tonosaki <manabu@tomarika.com>
2020-09-19 15:10:38 +0900

    Initial

---
+++ TalkLoggerWinform/FormMain.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace TalkLoggerWinform
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.contextMenuStripMain = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItemExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemFitToBottom = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.contextMenuStripMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStripMain
+            // 
+            this.contextMenuStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemFitToBottom,
+            this.toolStripSeparator1,
+            this.toolStripMenuItemExit});
+            this.contextMenuStripMain.Name = "contextMenuStripMain";
+            this.contextMenuStripMain.Size = new System.Drawing.Size(140, 54);
+            // 
+            // toolStripMenuItemExit
+            // 
+            this.toolStripMenuItemExit.Name = "toolStripMenuItemExit";
+            this.toolStripMenuItemExit.Size = new System.Drawing.Size(139, 22);
+            this.toolStripMenuItemExit.Text = "Exit";
+            this.toolStripMenuItemExit.Click += new System.EventHandler(this.toolStripMenuItem1Quit_Click);
+            // 
+            // toolStripMenuItemFitToBottom
+            // 
+            this.toolStripMenuItemFitToBottom.Name = "toolStripMenuItemFitToBottom";
+            this.toolStripMenuItemFitToBottom.Size = new System.Drawing.Size(139, 22);
+            this.toolStripMenuItemFitToBottom.Text = "FitToBottom";
+            this.toolStripMenuItemFitToBottom.Click += new System.EventHandler(this.toolStripMenuItemFitToBottom_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(136, 6);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BackgroundImage = global::TalkLoggerWinform.Properties.Resources.bg1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(624, 81);
+            this.ContextMenuStrip = this.contextMenuStripMain;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.Text = "Talk Logger";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.FormMain_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.FormMain_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.FormMain_MouseUp);
+            this.contextMenuStripMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStripMain;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemExit;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemFitToBottom;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+    }
+}
+
---
+++ TalkLoggerWinform/FormMain.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TalkLoggerWinform
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripMenuItem1Quit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private bool IsWindowMoving = false;
+        private Point DragStartLocationMouse;
+        private Point DragStartLocationForm;
+        private void FormMain_MouseDown(object sender, MouseEventArgs e)
+        {
+            DragStartLocationMouse = PointToScreen(e.Location);
+            DragStartLocationForm = Location;
+            IsWindowMoving = true;
+        }
+
+        private void FormMain_MouseMove(object sender, MouseEventArgs e)
+        {
+            var mpos = PointToScreen(e.Location);
+            
+            if (IsWindowMoving)
+            {
+                this.Location = new Point
+                {
+                    X = DragStartLocationForm.X + mpos.X - DragStartLocationMouse.X,
+                    Y = DragStartLocationForm.Y + mpos.Y - DragStartLocationMouse.Y,
+                };
+            }
+        }
+
+        private void FormMain_MouseUp(object sender, MouseEventArgs e)
+        {
+            IsWindowMoving = false;
+        }
+
+        private void toolStripMenuItemFitToBottom_Click(object sender, EventArgs e)
+        {
+            Location = new Point
+            {
+                X = Screen.PrimaryScreen.WorkingArea.Left,
+                Y = Screen.PrimaryScreen.WorkingArea.Bottom - Height,
+            };
+            Size = new Size
+            {
+                Width = Screen.PrimaryScreen.WorkingArea.Width,
+                Height = Height,
+            };
+        }
+    }
+}
