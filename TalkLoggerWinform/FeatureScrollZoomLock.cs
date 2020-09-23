583e6700d6e6b13ad325d909feb91133fa373b1f
Manabu Tonosaki <manabu@tomarika.com>
2020-09-23 22:15:30 +0900

    Usability Kaizen (Lock Scroll-Y, Zoom-Y)

---
+++ TalkLoggerWinform/FeatureScrollZoomLock.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tono.GuiWinForm;
+
+namespace TalkLoggerWinform
+{
+    public class FeatureScrollZoomLock : CoreFeatureBase, IMouseListener
+    {
+        public override void OnInitInstance()
+        {
+            base.OnInitInstance();
+            TarPane = Pane.GetPane("GuiViewMain");
+        }
+
+        public void OnMouseDown(MouseState e)
+        {
+        }
+
+        public void OnMouseMove(MouseState e)
+        {
+            Pane.Scroll = ScreenPos.FromInt(Pane.Scroll.X, 12);
+            Pane.Zoom = new XyBase { X = Pane.Zoom.X, Y = 1000, };
+        }
+
+        public void OnMouseUp(MouseState e)
+        {
+        }
+
+        public void OnMouseWheel(MouseState e)
+        {
+        }
+    }
+}
